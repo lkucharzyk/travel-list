8fd9bc28d8c76f366a4817bcb86534a5f3052068
Łukasze <lukaszkucharzyk17@gmail.com>
2023-09-19 09:58:12 +0200

    basic components structure setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-function Logo() {}
+function Logo() {
+  return <h1>🧳Far Away😎</h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already pack X</em>
+    </footer>
+  );
+}
